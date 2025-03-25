79690519ecf61d832505461983470f104e1a3b43
Govind29-rgb <govoffi29@gmail.com>
2025-03-25 17:23:15 +0530

    backend_allfine

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,7 +12,8 @@ app.use(express.json());
 app.use(cookieParser())
 import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
-import messageRoutes from "./routes/message.route.js";
+import messageRoutes from './routes/message.route.js'; 
+
 
 const PORT = process.env.PORT || 5000;
 
