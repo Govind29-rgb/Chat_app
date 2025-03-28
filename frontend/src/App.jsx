362b5ed44eaada54965aa5fca6493ff18bc22f99
Govind29-rgb <govoffi29@gmail.com>
2025-03-28 17:10:41 +0530

    routing_error_fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,23 @@
-import Navbar from "./components/navbar";
-import { Routes,Route } from "react-router-dom";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import ProfilePage from './pages/ProfilePage';
+import SettingsPage from './pages/SettingsPage';
+import SignUpPage from './pages/SignUpPage';
+import Navbar from './components/navbar';
 
 function App() {
   return (
     <div>
-      <Navbar/>
+      <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/signup" element={<SignUpPage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/settings" element={<SettingsPage />} />
         <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
       </Routes>
-
-   
     </div>
   );
 }
--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-const navbar = () => {
+function Navbar() {
   return (
-    <div>navbar</div>
-  )
+    <nav>
+      <Link to="/" className="btn btn-primary">Home</Link>
+      <Link to="/login" className="btn btn-primary">Login</Link>
+      <Link to="/profile" className="btn btn-primary">Profile</Link>
+      <Link to="/settings" className="btn btn-primary">Settings</Link>
+      <Link to="/signup" className="btn btn-primary">Sign Up</Link>
+    </nav>
+  );
 }
 
-export default navbar
+export default Navbar;
