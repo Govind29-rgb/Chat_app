bfb3a2c04cc82926a030ec5c16250f5a7add30cf
Govind29-rgb <govoffi29@gmail.com>
2025-03-28 16:58:58 +0530

    daisyui+pages_created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,19 @@
+import Navbar from "./components/navbar";
+import { Routes,Route } from "react-router-dom";
+
 function App() {
   return (
-    <div className="text-center">
-      <h1 className="text-3xl font-bold underline text-blue-500">
-        Hello, Tailwind CSS!
-      </h1>
+    <div>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+
+   
     </div>
   );
 }
