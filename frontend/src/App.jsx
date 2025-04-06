e5dadff14df90dc62e0cbf6e7f2ac6c6f7e563d0
Govind29-rgb <govoffi29@gmail.com>
2025-04-06 18:36:28 +0530

    signup and login completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,8 +6,12 @@ import LoginPage from './pages/LoginPage.jsx';
 import ProfilePage from './pages/ProfilePage.jsx';
 import SettingsPage from './pages/SettingsPage.jsx';
 import SignUpPage from './pages/SignUpPage.jsx';
-import { useAuthStore } from './store/useAuthStore.js'; // or '../store/authStore' depending on where the file is
+import { useAuthStore } from './store/useAuthStore.js';
+import { Navigate } from 'react-router-dom';
+
 import { Loader } from "lucide-react";
+import { Toaster } from "react-hot-toast";
+
 
 const App = () => {
   const {authUser,checkAuth,isCheckingAuth}=useAuthStore()
@@ -33,6 +37,7 @@ const App = () => {
         <Route path="/settings" element={<SettingsPage />} />
         <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
       </Routes>
+      <Toaster />
     </>
   );
 };
